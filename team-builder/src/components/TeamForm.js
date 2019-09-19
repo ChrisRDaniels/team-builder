4f5b1342116481267d730af4e223df813f627e2c
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-19 15:33:34 -0600

    final commit prior to styling

--- team-builder/src/components/TeamForm.js
+++ team-builder/src/components/TeamForm.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Team = props => {
     return (
         <>
-            {props.team.map(tm => (
-                <div className="tm" key={tm.id}>
-                    <h2>{tm.name}</h2>
-                    <p>{tm.email}</p>
-                    <p>{tm.role}</p>
+            {props.team.map(teamMember => (
+                <div className="teamMember" key={teamMember.id}>
+                    <h2>{teamMember.name}</h2>
+                    <p>{teamMember.email}</p>
+                    <p>{teamMember.role}</p>
                 </div>
             ))}
         </>
